bfcafbb135dbec6a4de80413d32556c104aae985
larissacsf <larissa_cuane10@hotmail.com>
2020-04-06 14:26:55 -0300

    First commit

---
+++ MoraisParkingJava/src/com/larissacsf/model/Veiculo.java
@@ -0,0 +1,10 @@
+package com.larissacsf.model;
+
+public class Veiculo {
+
+}
+
+
+
+
+
---
+++ MoraisParkingJava/src/com/larissacsf/view/Main.java
@@ -0,0 +1,11 @@
+package com.larissacsf.view;
+
+import com.larissacsf.model.Veiculo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
