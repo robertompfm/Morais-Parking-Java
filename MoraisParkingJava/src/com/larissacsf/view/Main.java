4817ebcafaa79436ba216e532cc48fb64d1417f8
larissacauane <61204154+larissacauane@users.noreply.github.com>
2020-04-06 14:55:27 -0300

    Update Main.java

--- MoraisParkingJava/src/com/larissacsf/view/Main.java
+++ MoraisParkingJava/src/com/larissacsf/view/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+	    Veiculo v1 = new Veiculo("Larissa", 20192007035, "Sistemas para Internet", "BAR7801", "Carro");
+	    System.out.println(v1.toString());
 
     }
 }
