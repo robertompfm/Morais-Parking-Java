6de1db60ef9538c0c03ba992d66590dd3bccd0c7
Roberto Mendes <robertompfm@gmail.com>
2020-04-22 15:29:46 -0300

    first commit

---
+++ MoraisParkingNB/src/main/java/moraisparkingnb/view/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moraisparkingnb.view;
+
+import moraisparkingnb.tests.Test;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Test.testEstacionamento();
+        
+    }
+    
+}
