5d747bac2cb1d5db4607966d8f6c0bc7aeb9f492
Roberto Mendes <robertompfm@gmail.com>
2020-05-03 17:05:33 -0300

    UI sidebar, topbar and content area

---
+++ MoraisParkingFX/src/control/SidebarController.java
@@ -0,0 +1,20 @@
+package control;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SidebarController implements Initializable {
+
+
+    @FXML
+    private VBox sidebar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
--- MoraisParkingFX/src/module-info.java
+++ MoraisParkingFX/src/module-info.java
@@ -1,8 +1,14 @@
 module MoraisParkingFX {
     requires javafx.fxml;
     requires javafx.controls;
+    requires javafx.graphics;
     requires java.sql;
+    requires fontawesomefx;
 
     opens view;
     opens control;
+    opens model;
+    opens view.fxml;
+    opens view.css;
+    opens model.data;
 }
