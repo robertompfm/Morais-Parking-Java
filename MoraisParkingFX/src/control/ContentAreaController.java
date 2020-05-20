2a0efff7984eab1c1b2d7fe74727cac8b4e5dd7b
Roberto Mendes <robertompfm@gmail.com>
2020-05-20 10:56:23 -0300

    logout done

--- MoraisParkingFX/src/control/ContentAreaController.java
+++ MoraisParkingFX/src/control/ContentAreaController.java
@@ -1,13 +1,19 @@
 package control;
 
+import dao.DataUsuarios;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import model.Usuario;
 
 import java.io.IOException;
 import java.net.URL;
@@ -17,6 +23,9 @@ public class ContentAreaController implements Initializable {
 
     @FXML
     private VBox contentArea;
+
+    @FXML
+    private Button openMenuBtn;
 
     private boolean opened = false;
 
@@ -36,5 +45,18 @@ public class ContentAreaController implements Initializable {
             borderPane.setLeft(null);
             opened = false;
         }
+    }
+
+    @FXML
+    public void logout(ActionEvent event) throws IOException {
+        openMenuBtn.getScene().getWindow().hide();
+        Parent root = FXMLLoader.load(getClass().getResource("/view/fxml/login.fxml"));
+        Stage primaryStage = new Stage();
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+        DataUsuarios dataUsuarios = DataUsuarios.getInstance();
+        dataUsuarios.setCurrentUser(null);
     }
 }
